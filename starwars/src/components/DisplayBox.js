6bf22e1854c1d3818e94206185aa665a01335647
jacobdurst <jdd2007@gmail.com>
2019-05-20 15:07:54 -0400

    imported individual character boxes into displaybox

--- starwars/src/components/DisplayBox.js
+++ starwars/src/components/DisplayBox.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import '../components/StarWars.css'
+import IndivCharacter from './IndivCharacter';
 
 const DisplayBox = props => { 
     return (
         <div className="box">
             <h1>Here is my display box</h1>
+            <IndivCharacter />
+            <IndivCharacter />
+            <IndivCharacter />
         </div>
     );
 };
---
+++ starwars/src/components/IndivCharacter.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './StarWars.css';
+
+const IndivCharacter = (props) => {
+    return (
+        <div className="indivCharacter">
+            <h2>Here is an individual Star Wars character</h2>
+        </div>
+    )
+};
+
+export default IndivCharacter
