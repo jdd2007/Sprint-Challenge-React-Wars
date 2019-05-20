f9b1cc9026c3ac347230bae0d94f8152f5adaf9e
jacobdurst <jdd2007@gmail.com>
2019-05-20 14:42:40 -0400

    barebones display box added

---
+++ starwars/src/components/DisplayBox.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import '../components/StarWars.css'
+
+const DisplayBox = props => { 
+    return (
+        <div className="box">
+            <h1>Here is my display box</h1>
+        </div>
+    );
+};
+export default DisplayBox;
